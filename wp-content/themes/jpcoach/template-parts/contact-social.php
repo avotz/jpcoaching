1e1e6a750acc458d6962151dc2c234a720ed6178
Alonso <alons182@hotmail.com>
2015-08-31 10:24:36 -0600

    se agrego skype face y gogle socia

--- wp-content/themes/jpcoach/template-parts/contact-social.php
+++ wp-content/themes/jpcoach/template-parts/contact-social.php
@@ -1,6 +1,6 @@
 <div class="contact__content__social">
-    <a href="#" class="contact__content__social__link"><i class="icon-facebook"></i></a>
+    <a href="https://www.facebook.com/gestiondeproyectosjp?fref=ts" class="contact__content__social__link" target="_blank"><i class="icon-facebook"></i></a>
      <a href="#" class="contact__content__social__link"><i class="icon-twitter"></i></a>
-     <a href="#" class="contact__content__social__link"><i class="icon-google-plus"></i></a>
-     <a href="#" class="contact__content__social__link"><i class="icon-youtube"></i></a>
+     <a href="https://plus.google.com/u/0/115784436326464294306/posts" class="contact__content__social__link" target="_blank"><i class="icon-google-plus"></i></a>
+     <a href="skype:jpcoachgroup" class="contact__content__social__link"><i class="icon-skype"></i></a>
 </div>
