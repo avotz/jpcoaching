2a81d166d384b742ae64ae518ef5322ff990750f
Alonso <alons182@hotmail.com>
2015-08-31 09:19:06 -0600

    se cambiaron las imagenes del banner

--- wp-content/themes/jpcoach/page-home.php
+++ wp-content/themes/jpcoach/page-home.php
@@ -20,6 +20,9 @@ get_header(); ?>
                      <div class="banner__slide" >
                         <img src="<?php echo get_template_directory_uri(); ?>/img/banner2.jpg" alt="Banner" />
                     </div>
+                    <div class="banner__slide" >
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/banner3.jpg" alt="Banner" />
+                    </div>
                 
                     
                 </div>       
