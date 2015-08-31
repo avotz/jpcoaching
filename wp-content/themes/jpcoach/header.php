d18a5b4ba86f3357644d3e228f2e7d1fa7abaf5b
Alonso <alons182@hotmail.com>
2015-08-31 10:32:10 -0600

    se agrego twitter social

--- wp-content/themes/jpcoach/header.php
+++ wp-content/themes/jpcoach/header.php
@@ -29,7 +29,7 @@
         <div class="top__social">
 
              <a href="https://www.facebook.com/gestiondeproyectosjp?fref=ts" class="top__social__link" target="_blank"><i class="icon-facebook"></i></a>
-             <a href="#" class="top__social__link"><i class="icon-twitter"></i></a>
+             <a href="https://twitter.com/JPCoachGroup" class="top__social__link" target="_blank"><i class="icon-twitter"></i></a>
              <a href="https://plus.google.com/u/0/115784436326464294306/posts" class="top__social__link" target="_blank"><i class="icon-google-plus"></i></a>
              <a href="skype:jpcoachgroup" class="top__social__link"><i class="icon-skype"></i></a>
                      
--- wp-content/themes/jpcoach/template-parts/contact-social.php
+++ wp-content/themes/jpcoach/template-parts/contact-social.php
@@ -1,6 +1,6 @@
 <div class="contact__content__social">
     <a href="https://www.facebook.com/gestiondeproyectosjp?fref=ts" class="contact__content__social__link" target="_blank"><i class="icon-facebook"></i></a>
-     <a href="#" class="contact__content__social__link"><i class="icon-twitter"></i></a>
+     <a href="https://twitter.com/JPCoachGroup" class="contact__content__social__link" target="_blank"><i class="icon-twitter"></i></a>
      <a href="https://plus.google.com/u/0/115784436326464294306/posts" class="contact__content__social__link" target="_blank"><i class="icon-google-plus"></i></a>
      <a href="skype:jpcoachgroup" class="contact__content__social__link"><i class="icon-skype"></i></a>
 </div>
