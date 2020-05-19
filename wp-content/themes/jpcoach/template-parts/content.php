65412064dda6b1bbb380ee767c3262df6a865e87
Alonso Chavarria <alonso@avotz.com>
2020-05-18 20:22:08 -0600

    layout blog

--- wp-content/themes/jpcoach/template-parts/content.php
+++ wp-content/themes/jpcoach/template-parts/content.php
@@ -13,12 +13,41 @@
 	<header class="entry-header">
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
+		<?php if (has_post_thumbnail()) :
+
+			$id = get_post_thumbnail_id($post->ID);
+			$big_url = wp_get_attachment_image_src($id, 'item-banner', true);
+			$thumb_url = wp_get_attachment_image_src($id, 'large', true);
+			?>
+		
+		<?php if (wp_is_mobile()) { ?>
+			<figure class="post-banner blog-banner" style="background-image: url('<?php echo $thumb_url[0] ?>');">
+			<?php if (!is_single()) : ?>
+				<a href="<?php echo get_permalink() ?>"></a>
+			
+			<?php endif; ?>
+		</figure>
+
+		<?php 
+			} else { ?>
+					<figure class="post-banner blog-banner" style="background-image: url('<?php echo $big_url[0] ?>');">
+						<?php if (!is_single()) : ?>
+							<a href="<?php echo get_permalink() ?>"></a>
+						
+						<?php endif; ?>
+					</figure>
+					<?php 
+			} ?>
+
+		<?php endif; ?>
+		
 		<?php if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
 			<?php jpcoach_posted_on(); ?>
 		</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
+
 
 	<div class="entry-content">
 		<?php
